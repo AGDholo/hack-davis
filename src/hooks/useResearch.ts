a3077007b3df340587ccaeecbe6ee7cbab6669e0
Guyang Li <agdholo@outlook.com>
2024-04-28 13:34:17 -0700

    Refactor useResearch hook to include applied field in allResearchesWithApplied

--- src/hooks/useResearch.ts
+++ src/hooks/useResearch.ts
@@ -1,11 +1,9 @@
 import useSWR from "swr";
 import {Research} from "./useUser.ts";
 import {useJwtFetcher} from "./useJwtFetcher.ts";
-import {useAuthInfo} from "@propelauth/react";
 
 export const useResearch = () => {
     const jwtFetcher = useJwtFetcher();
-    const authInfo = useAuthInfo()
     const {
         data: allResearches,
         isLoading: isAllResearchesLoading
@@ -13,24 +11,24 @@ export const useResearch = () => {
         refreshInterval: 1000 * 10
     });
 
-    const {
-        data: allResearchesByApplied
-    } = useSWR(authInfo.accessToken && '/proxy/research/list-by-apply', jwtFetcher, {
-        refreshInterval: 1000 * 10
-    });
+    // const {
+    //     data: allResearchesByApplied
+    // } = useSWR(authInfo.accessToken && '/proxy/research/list-by-apply', jwtFetcher, {
+    //     refreshInterval: 1000 * 10
+    // });
+    //
+    // // get a new research list from all researches and user researches, apply a new field:applied to each research object
+    // // if the research is in the user researches list, applied is true, otherwise false
+    //
+    // const allResearchesWithApplied = allResearchesByApplied && allResearches?.map(research => {
+    //     return {
+    //         ...research,
+    //         applied: allResearchesByApplied?.some((userResearch: {
+    //             research_id: string;
+    //         }) => userResearch.research_id === research.id)
+    //     }
+    // });
 
-    // get a new research list from all researches and user researches, apply a new field:applied to each research object
-    // if the research is in the user researches list, applied is true, otherwise false
 
-    const allResearchesWithApplied = allResearchesByApplied && allResearches?.map(research => {
-        return {
-            ...research,
-            applied: allResearchesByApplied?.some((userResearch: {
-                research_id: string;
-            }) => userResearch.research_id === research.id)
-        }
-    });
-
-
-    return {allResearches, isAllResearchesLoading, allResearchesWithApplied};
+    return {allResearches, isAllResearchesLoading};
 }
