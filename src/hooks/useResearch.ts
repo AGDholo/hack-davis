0488aec8ca6ee77bb345ea826ced98fd07464df4
Guyang Li <agdholo@outlook.com>
2024-04-28 13:32:28 -0700

    Refactor useResearch hook to include applied field in allResearchesWithApplied

--- src/hooks/useResearch.ts
+++ src/hooks/useResearch.ts
@@ -22,17 +22,14 @@ export const useResearch = () => {
     // get a new research list from all researches and user researches, apply a new field:applied to each research object
     // if the research is in the user researches list, applied is true, otherwise false
 
-    let allResearchesWithApplied = []
-    if (authInfo.accessToken && allResearchesByApplied && allResearchesByApplied.length > 0 && allResearches && allResearches.length > 0) {
-        allResearchesWithApplied = allResearchesByApplied && allResearches?.map(research => {
-            return {
-                ...research,
-                applied: allResearchesByApplied?.some((userResearch: {
-                    research_id: string;
-                }) => userResearch.research_id === research.id)
-            }
-        });
-    }
+    const allResearchesWithApplied = allResearchesByApplied && allResearches?.map(research => {
+        return {
+            ...research,
+            applied: allResearchesByApplied?.some((userResearch: {
+                research_id: string;
+            }) => userResearch.research_id === research.id)
+        }
+    });
 
 
     return {allResearches, isAllResearchesLoading, allResearchesWithApplied};
--- src/pages/dashboard/job/Research.tsx
+++ src/pages/dashboard/job/Research.tsx
@@ -1,23 +1,13 @@
-import {Key} from "react";
 import {JobCard} from "../../../components/job/Jobcard.tsx";
 import {useResearch} from "../../../hooks/useResearch.ts";
 
 export const Research = () => {
-    const {allResearchesWithApplied} = useResearch()
+    const {allResearches} = useResearch()
     return (
         <>
             <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-9 gap-4'}>
                 {
-                    allResearchesWithApplied && allResearchesWithApplied?.map((job: {
-                        id: string | undefined;
-                        applied: boolean | undefined;
-                        description: string;
-                        title: string;
-                        univercity: string;
-                        money: { toString: () => string; };
-                        isFullTime: boolean;
-                        location: string;
-                    }, index: Key | null | undefined) => (
+                    allResearches?.map((job, index) => (
                         <JobCard key={index}
                                  id={job.id}
                                  applied={job.applied}
