8b29e36aa66aa95c9cad05ac21d4481cea217b47
Guyang Li <agdholo@outlook.com>
2024-04-28 06:27:13 -0700

    Update useUser hook and related changes for user profile functionality

--- src/pages/dashboard/job/CreateResearch.tsx
+++ src/pages/dashboard/job/CreateResearch.tsx
@@ -1,92 +1,142 @@
 import {FormControlLabel, Switch, TextField} from "@mui/material";
-import {TextareaAutosize} from "@mui/base";
-import {FC, useRef} from "react";
+import {useRef, useState} from "react";
 import {Editor} from "@tinymce/tinymce-react";
 import {Editor as TinyMCEEditor} from "tinymce";
+import axios from "axios";
+import {useAuthInfo} from "@propelauth/react";
 
 export const CreateResearch = () => {
-    return (
-        <div className="container mx-auto max-w-lg space-y-4">
-            <div>
-                <h1 className="text-2xl font-bold">Create Research</h1>
-            </div>
-
-            <form className={'space-y-5'}>
-                <TextField id="outlined-basic"
-                           fullWidth
-                           label="Research Title"
-                           variant="outlined"/>
-                <TextareaAutosize
-                    minRows={3}
-                    className="w-full text-sm font-normal font-sans leading-normal p-3 rounded-xl rounded-br-none shadow-lg shadow-slate-100 dark:shadow-slate-900 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus:shadow-lg border border-solid border-slate-300 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 dark:border-slate-600 bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-300 focus-visible:outline-0 box-border"
-                    aria-label="empty textarea"
-                    placeholder="Description"
-                />
-
-                <UserEditor/>
-
-                <TextField id="outlined-basic"
-                           fullWidth
-                           label="Salary"
-                           variant="outlined"/>
-                <TextField id="outlined-basic"
-                           fullWidth
-                           label="Localtion"
-                           variant="outlined"/>
-                <TextField id="outlined-basic"
-                           fullWidth
-                           label="University"
-                           variant="outlined"/>
-
-                <div className={'flex justify-between'}>
-                    <FormControlLabel control={<Switch/>}
-                                      label="Fulltime"/>
-
-                    <div>
-                        <button className="hover:bg-cyan-500 hover:text-white rounded-3xl px-5 py-1 outline">
-                            Create
-                        </button>
-                    </div>
-                </div>
-            </form>
-
-        </div>
-    )
-}
-
-const UserEditor: FC = () => {
     const editorRef = useRef<TinyMCEEditor | null>(null);
+    const authInfo = useAuthInfo();
+    const [createSuccess, setCreateSuccess] = useState(false);
 
-    const log = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        money: '',
+        location: '',
+        univercity: '',
+        isfulltime: false,
+        description: ''
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value, type, checked} = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === 'checkbox' ? checked : value
+        });
+    };
+
+    const handleEditorChange = () => {
         if (editorRef.current) {
-            console.log(editorRef.current.getContent());
+            setFormData({
+                ...formData,
+                description: editorRef.current.getContent()
+            });
         }
     };
 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            await axios.post(
+                `${import.meta.env.VITE_APP_BACKEND_URL}/research/create`,
+                {
+                    Research: {
+                        ...formData
+
+                    }
+                },
+                {
+                    headers: {Authorization: `Bearer ${authInfo.accessToken}`}
+                }
+            );
+
+            setCreateSuccess(true)
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     return (
-        <>
-            <Editor
-                apiKey={'b4bz61z0lpmf3j3k1u8z3fqbs8tjoci5o6cwhoq0tytl1uws'}
-                //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                onInit={(_evt, editor) => (editorRef.current = editor)} // This is how it should be done
-                initialValue="<p>This is the initial content of the editor.</p>"
-                init={{
-                    height: 500,
-                    menubar: false,
-                    plugins: [
-                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-                    ],
-                    toolbar: 'undo redo | blocks | ' +
-                        'bold italic forecolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
-                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                }}
-            />
-            <button onClick={log}>Log editor content</button>
-        </>
+        <div className="container mx-auto max-w-lg space-y-4">
+            {createSuccess && (
+                <div className="bg-green-100 text-green-500 rounded-3xl p-4">
+                    Research created successfully
+                </div>
+            )}
+            <h1 className="text-2xl font-bold">Create Research</h1>
+            <form className="space-y-5"
+                  onSubmit={handleSubmit}>
+                <TextField
+                    fullWidth
+                    label="Research Title"
+                    variant="outlined"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleInputChange}
+                />
+
+                <Editor
+                    apiKey="b4bz61z0lpmf3j3k1u8z3fqbs8tjoci5o6cwhoq0tytl1uws"
+                    //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-expect-error
+                    onInit={(_evt, editor) => editorRef.current = editor}
+                    initialValue="<p>Fill your research description</p>"
+                    init={{
+                        height: 300,
+                        menubar: false,
+                        setup: editor => {
+                            editor.on('Change', handleEditorChange);
+                        }
+                    }}
+                />
+
+                <TextField
+                    fullWidth
+                    label="Salary/M"
+                    type="number"
+                    variant="outlined"
+                    name="money"
+                    value={formData.money}
+                    onChange={handleInputChange}
+                />
+
+                <TextField
+                    fullWidth
+                    label="Location"
+                    variant="outlined"
+                    name="location"
+                    value={formData.location}
+                    onChange={handleInputChange}
+                />
+
+                <TextField
+                    fullWidth
+                    label="University"
+                    variant="outlined"
+                    name="univercity"
+                    value={formData.univercity}
+                    onChange={handleInputChange}
+                />
+
+
+                <div className={'flex justify-between'}>
+                    <FormControlLabel
+                        control={<Switch checked={formData.isfulltime}
+                                         onChange={handleInputChange}
+                                         name="isfulltime"/>}
+                        label="Fulltime"
+                    />
+
+                    <button className="hover:bg-cyan-500 hover:text-white rounded-3xl px-5 py-1 outline">
+                        Create
+                    </button>
+                </div>
+            </form>
+        </div>
     );
-};
+}
+
